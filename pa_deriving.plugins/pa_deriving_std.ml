814c8d88aa0a51a05c7169560cdd44397e1d865d
Chet Murthy <chetsky@gmail.com>
2025-02-19 23:23:41 -0800

    fix loading of pa_ppx.deriving_plugins.std

---
+++ pa_deriving.plugins/pa_deriving_std.ml
@@ -0,0 +1,10 @@
+(**pp -syntax camlp5r *)
+
+open Pa_deriving_show;
+open Pa_deriving_eq;
+open Pa_deriving_ord;
+open Pa_deriving_enum;
+open Pa_deriving_iter;
+open Pa_deriving_map;
+open Pa_deriving_fold;
+open Pa_deriving_make;
